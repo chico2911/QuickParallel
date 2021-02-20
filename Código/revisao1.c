f1f773aeaf75480d87591a24beb8d8b2af48d62c
Francisco Lopes <a85367@alunos.uminho.pt>
2021-02-20 23:03:17 +0000

    Added Parallel Quicksort

---
+++ Código/revisao1.c
@@ -0,0 +1,78 @@
+#include<stdio.h>
+#include<omp.h>
+#include<math.h>
+#include <stdlib.h>
+#define ARRAY_MAX_SIZE 500000
+
+void serial_quicksort(int *array, int lo, int hi){
+	int i=lo,j=hi,h;
+   	int x=array[(lo+hi)/2];
+    //partition
+    do  {
+        while(array[i]<x) i++;
+        while(array[j]>x) j--;
+        if(i<=j){
+        h=array[i]; array[i]=array[j]; array[j]=h;
+        i++; j--;
+        }
+    }while(i<=j);
+    //recursion
+    if(lo<j) serial_quicksort(array,lo,j);
+    if(i<hi) serial_quicksort(array,i,hi);
+}
+
+void quicksort(int * array, int lo, int hi){
+	int pivotElement;
+    //Se o array for menor que 10000, o custo de atribuir threads não compensa, logo executará em modo sequencial
+	if((hi - lo + 1) < 10000) {
+		serial_quicksort(array, lo, hi);
+	} 
+    else {
+		int i=lo,j=hi,h;
+   		int x=array[(lo+hi)/2];
+		do{
+       		while(array[i]<x) i++;
+        	while(array[j]>x) j--;
+        	if(i<=j){
+        		h=array[i]; array[i]=array[j]; array[j]=h;
+        		i++; j--;
+        	}
+    	}while(i<=j);
+		#pragma omp task 
+		{
+			quicksort(array,lo,j);
+		}
+		#pragma omp task 
+		{
+			quicksort(array,i,hi);
+		}
+	}
+}
+
+void readFile(FILE *fp,int *array, int arraySize){
+    for( int i = 0; i < arraySize-1; i++ ){
+        fscanf (fp, "%d", &array[i]);
+    }
+}
+
+int main(int argc, char* argv[])
+{
+    //Open the file where the array is at
+    FILE *fp = fopen(argv[1],"r");
+    int arraySize = atoi(argv[2]);
+	int arr[ARRAY_MAX_SIZE];
+    //Read the values from file do array arr
+    readFile(fp,arr,arraySize);
+    double start_time, run_time;
+    // See Beginning time
+    start_time = omp_get_wtime();
+    //Make QuickSort
+    #pragma omp parallel
+	{
+	quicksort(arr, 0, arraySize-1);
+	}
+    //Beginning time - Final time
+    run_time = omp_get_wtime() - start_time;
+	printf("Run time was %lf seconds\n ",run_time);
+	return 0; 
+} 
---
+++ README.md
@@ -0,0 +1,21 @@
+# Parallel Quicksort with OpenMP
+## Projeto Inserido na Unidade Curricular Computação Paralela 2020/2021
+### [Mestrado Integrado em Engenhária Informática na Universidade do Minho](https://miei.di.uminho.pt/)
+
+Este projeto consistia na paralelização do algortimo **Quicksort** utilizando diretivas do **OpenMP**. Desta forma é possível através da atribuição de threads para cada recursividade da implementação, assim existirá, dentro de certas, condições um speed up do desempenho do algoritmo.
+Nos [anexos](https://github.com/chico2911/QuickParallel/blop/master/Docs/anexos.xlsx) encontram-se uma série de testes efetuados para apurar o ganho. A estratégia utilizada é abordada e detalhada no [relatório](https://github.com/chico2911/QuickParallel/blop/master/Docs/relatorio_quicksort_a85367_a84092.pdf).
+
+Na seguinte imagem podemos ver uma comparação entre a variação dos tempos de execução nas diferentes máquinas utilizadas. Os valores apresentados são em segundos(y), bem como o array ordenado é o [array1](https://github.com/chico2911/QuickParallel/blop/master/Inputs/array1.txt) que conta com 500k entradas.
+
+<p align="center">
+   <img width="510" height="159" src="https://github.com/chico2911/Civere/blob/master/Docs/graph.jpg">
+</p>
+
+### [Relatório](https://github.com/chico2911/QuickParallel/blop/master/Docs/relatorio_quicksort_a85367_a84092.pdf)
+
+### [Apresentação](https://github.com/chico2911/QuickParallel/blop/master/Docs/apresentacao.pdf)
+
+## Development Team
+
+* [Diogo Pereira](https://github.com/dpereira7)
+* [Francisco Lopes](https://github.com/chico2911)
